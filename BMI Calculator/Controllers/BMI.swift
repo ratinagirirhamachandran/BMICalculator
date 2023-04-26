a13b3f99705d0ea6ddb85011e3c477faa23b2d65
Ratinagiri Rhamachandran <ratinagiri1989@gmail.com>
2023-04-25 21:20:23 -0400

    BMI Calculator

---
+++ BMI Calculator/Controllers/BMI.swift
@@ -0,0 +1,16 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Ratinagiri Rhamachandran on 11/4/16.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
+
+
---
+++ BMI Calculator/Controllers/ResultViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ResultViewController.swift
+//  BMI Calculator
+//
+//  Created by Ratinagiri Rhamachandran on 11/4/16.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    
+    
+    var bmiValue: String?
+    var advice: String?
+    var color: UIColor?
+
+    @IBOutlet weak var bmiLabel: UILabel!
+    @IBOutlet weak var adviceLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        bmiLabel.text = bmiValue
+        adviceLabel.text = advice
+        view.backgroundColor = color
+        
+    }
+    
+    
+    @IBAction func recalculatedPressed(_ sender: UIButton) {
+        self.dismiss(animated: true)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
